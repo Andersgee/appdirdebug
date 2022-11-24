2b41ef24df22b7569f9b28abd2c7bbac6e87f364
Anders Gustafsson <andersgee@gmail.com>
2022-11-24 13:41:54 +0100

    lib/env

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,6 @@
+//avoid invalid env vars
+!process.env.SKIP_ENV_VALIDATION && (await import("./lib/env/server.mjs"));
+
 /** @type {import('next').NextConfig} */
 const config = {
   experimental: {
