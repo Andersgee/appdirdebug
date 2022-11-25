982e2b986ad6264385c8dec065a2e6d968941a05
Anders Gustafsson <andersgee@gmail.com>
2022-11-25 16:15:23 +0100

    theme respond to system change

--- lib/utils/theme.ts
+++ lib/utils/theme.ts
@@ -1,26 +1,24 @@
-/**
- * the string that [next-themes](https://github.com/pacocoursey/next-themes)
- * used for `attribute="class"`
- */
-export const THEME_SCRIPT_STRING = `!(function () {
-  try {
-      var d = document.documentElement,
-          c = d.classList
-      c.remove('light', 'dark')
-      var e = localStorage.getItem('theme')
-      if ('system' === e || (!e && true)) {
-          var t = '(prefers-color-scheme: dark)',
-              m = window.matchMedia(t)
-          if (m.media !== t || m.matches) {
-              d.style.colorScheme = 'dark'
-              c.add('dark')
-          } else {
-              d.style.colorScheme = 'light'
-              c.add('light')
-          }
-      } else if (e) {
-          c.add(e || '')
-      }
-      if (e === 'light' || e === 'dark') d.style.colorScheme = e
-  } catch (e) {}
-})()`;
+export const THEME_SCRIPT_STRING = `
+var m = window.matchMedia('(prefers-color-scheme: dark)')
+var c = document.documentElement.classList
+
+if (localStorage.theme === 'dark' || (m.matches && !('theme' in localStorage))) {
+    c.add('dark')
+} else {
+    c.remove('dark')
+}
+
+function setColorScheme(e) {
+    if (e.matches) {
+        c.remove('light')
+        c.add('dark')
+        localStorage.setItem('theme', 'dark')
+    } else {
+        c.remove('dark')
+        c.add('light')
+        localStorage.setItem('theme', 'light')
+    }
+}
+
+m.addEventListener('change', setColorScheme)
+`;
